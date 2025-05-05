0429fc362ce39b1c4a021332ab6b623cc7612ebe
Mohammad Sheikh Shahinur Rahman <62345450+shahin0075@users.noreply.github.com>
2025-05-05 18:02:31 +0600

    update

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    darkMode: 'class', // Enable dark mode via class
+    theme: {
+      extend: {
+        fontFamily: {
+          sans: ['Montserrat', 'sans-serif'],
+        },
+      },
+    },
+    
+  }
+  
